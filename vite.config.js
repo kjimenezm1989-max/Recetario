8b2398a7fcc94d5b269c2a1f7a271b5e8c564f28
Kelvin Nicolas Jimenez Manga <kjimenezm1989@gmail.com>
2026-07-11 13:11:54 +0000

    Use relative paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ command }) => {
   const isBuild = command === 'build'
-  const base = isBuild ? '/Recetario/' : '/'
+  const base = isBuild ? './' : '/'
 
   return {
     base,
