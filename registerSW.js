2d6cf73f5b5d00f0ca96c562fb40324cf3b3cf78
Kelvin Nicolas Jimenez Manga <kjimenezm1989@gmail.com>
2026-07-11 13:08:18 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Recetario/sw.js', { scope: '/Recetario/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
